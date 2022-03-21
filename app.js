0f122eb2e3ec3413d0de362feb431bb52385a97c
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2022-03-21 14:14:20 +0100

    Linter & formatter added

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.get('/hoi', (req, res) => {
+    res.send('hoi')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port http://localhost:${port}`)
 })
