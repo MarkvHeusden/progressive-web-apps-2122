d991f7e9a1fce1f2600ffa5333e400c251b530e6
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2022-04-05 11:12:51 +0200

    Service worker v1

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,5 +1,5 @@
-const staticCache = 'site-static'
-const dynamicCache = 'site-dynamic'
+const staticCache = 'site-static-v2'
+const dynamicCache = 'site-dynamic-v2'
 const staticAssets = [
     '/',
     '/offline',
@@ -12,6 +12,7 @@ const staticAssets = [
     '/img/warn.svg',
     '/img/error.svg',
     '/img/search.svg',
+    '/img/barcode.png',
     'https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&family=Poppins:wght@700&display=swap',
     'https://fonts.gstatic.com/s/opensans/v28/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsgH1x4gaVQUwaEQbjA.woff',
     'https://fonts.gstatic.com/s/poppins/v19/pxiByp8kv8JHgFVrLCz7Z1xlFd2JQEk.woff2',
@@ -19,11 +20,15 @@ const staticAssets = [
 
 // Install event
 self.addEventListener('install', (event) => {
+    console.log('sw installed')
     // Open/create new static cache
     event.waitUntil(
-        caches.open(staticCache).then((cache) => {
-            cache.addAll(staticAssets)
-        }),
+        caches
+            .open(staticCache)
+            .then((cache) => {
+                cache.addAll(staticAssets)
+            })
+            .then(() => self.skipWaiting()),
     )
 })
 
@@ -32,54 +37,108 @@ self.addEventListener('activate', (event) => {
     // Delete old cache
     event.waitUntil(
         caches.keys().then((keys) => {
-            return Promise.all(keys.filter((key) => key !== staticCache && key !== dynamicCache).map((key) => caches.delete(key)))
-            // return Promise.resolve(
-            //     keys.forEach((key) => {
-            //         if (key !== staticCache) {
-            //             caches.delete(key)
-            //         }
-            //     }),
-            // )
+            return Promise.all(
+                keys
+                    .filter((key) => {
+                        return key !== staticCache && key !== dynamicCache
+                    })
+                    .map((key) => {
+                        return caches.delete(key)
+                    }),
+            )
         }),
     )
+    event.waitUntil(clients.claim())
 })
 
 // Fetch event
 self.addEventListener('fetch', (event) => {
-    console.log('Fetching:' + event.request.url)
-
-    // show cached request from cache
+    // console.log(event.request.url)
     event.respondWith(
+        // Check if requested data is already in cache
         caches.match(event.request).then((cachedResponse) => {
             if (cachedResponse) {
+                // If it is, use cached data
                 return cachedResponse
             } else {
+                // Else fetch the new data
                 return fetch(event.request)
-                    .then((fetchRes) => fetchRes)
+                    .then((fetchResponse) => {
+                        console.log('fs = ', fetchResponse)
+                        // Save new data in dynamic cache
+                        caches.open(dynamicCache).then((cache) => {
+                            cache.put(event.request.url, fetchResponse.clone())
+                            return fetchResponse
+                        })
+                    })
                     .catch(() => {
+                        // Show offline page if fetch failed
                         return caches.open(staticCache).then((cache) => cache.match('/offline'))
                     })
             }
         }),
     )
 
-    // Check stored cache before requesting new data, then save new data
     // event.respondWith(
-    //     caches
-    //         .match(event.request)
-    //         .then((cacheResponse) => {
-    //             return (
-    //                 cacheResponse ||
-    //                 fetch(event.request).then((fetchResponse) => {
-    //                     return caches.open(dynamicCache).then((cache) => {
-    //                         cache.put(event.request.url, fetchResponse.clone())
-    //                         return fetchResponse
-    //                     })
-    //                 })
-    //             )
-    //         })
-    //         .catch(() => {
-    //             return caches.match('/offline')
-    //         }),
+    //     (async () => {
+    //         const cachedResponse = await caches.match(event.request)
+    //         if (cachedResponse) {
+    //             return cachedResponse
+    //         }
+
+    //         const response = await fetch(event.request)
+    //         console.log('response;', response)
+    //         if (response) {
+    //             console.log('geen respons')
+    //             return response
+    //         } else {
+    //             return caches.open(staticCache).then((cache) => cache.match('/offline'))
+    //         }
+
+    // if (dynamicCache) {
+    //     const cache = await caches.open(dynamicCache)
+    //     await cache.put(event.request, response.clone())
+    // }
+    // return response
+    //     })(),
     // )
 })
+
+// event.respondWith(
+//     (async () => {
+//         const cachedResponse = await caches.match(event.request)
+//         if (cachedResponse) {
+//             return cachedResponse
+//         }
+
+//         await fetch(event.request)
+//             .then((fetchResponse) => {
+//                 console.log('fs = ', fetchResponse)
+//                 // Save new data in dynamic cache
+//                 caches.open(dynamicCache).then((cache) => {
+//                     cache.put(event.request.url, fetchResponse.clone())
+//                     return fetchResponse
+//                 })
+//             })
+//             .catch(() => {
+//                 // Show offline page if fetch failed
+//                 return caches.open(staticCache).then((cache) => cache.match('/offline'))
+//             })
+
+// const fetchResponse = await fetch(event.request)
+
+// if (fetchResponse) {
+//     console.log('fs = ', fetchResponse)
+//     // Save new data in dynamic cache
+//     caches.open(dynamicCache).then((cache) => {
+//         cache.put(event.request.url, fetchResponse.clone())
+//         return fetchResponse
+//     })
+// } else {
+//     // Show offline page if fetch failed
+//     return caches.open(staticCache).then((cache) => cache.match('/offline'))
+// }
+
+// return fetchResponse
+// })(),
+// )
